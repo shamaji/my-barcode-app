47b937889c179addda989bba9eb9587750de82bf
shamaji <chavadashamaji@gmail.com>
2023-04-14 12:10:35 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import Barcode from 'react-barcode';
+import { useState } from 'react';
+import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
 
 function App() {
+  const [barcodeName, setBarcodeName] = useState('');
+  const [barcodePrice, setBarcodePrice] = useState('');
+  const [isShowBarcode, setIsShowBarcode] = useState(false);
+
+  const saveBarcode = () => {
+    if (barcodeName && barcodePrice) {
+      setIsShowBarcode(true);
+    }
+  }
+
+  const addNewBarcode = () => {
+    setBarcodeName('');
+    setBarcodePrice('');
+    setIsShowBarcode(false);
+  }
+
+  const printBarcode = () => {
+    let printContents = document.getElementById('printBarcodediv').innerHTML;
+    let originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  }
+
+  const downloadBarcode = async () => {
+    const pricingTableElmt = document.getElementById('printBarcodediv');
+    if (!pricingTableElmt) return;
+
+    const canvas = await html2canvas(pricingTableElmt);
+    const dataURL = canvas.toDataURL('image/png');
+    downloadjs(dataURL, 'barcode.png', 'image/png');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+      {isShowBarcode ?
+        <Col>
+          <h1>Barcode Display</h1>
+          <div className='d-flex' style={{justifyContent:'space-around'}}>
+            <Button variant="primary" type="button" onClick={addNewBarcode}>
+              Add New Barcode
+            </Button>
+            <Button variant="primary" type="button" onClick={printBarcode}>
+              Print Barcode
+            </Button>
+            <Button variant="primary" type="button" onClick={downloadBarcode}>
+              Download Barcode
+            </Button>
+          </div>
+          <div id="printBarcodediv">
+            <Barcode value={barcodeName + '-' + barcodePrice} />
+          </div>
+        </Col>
+        :
+        <Col>
+          <h1>Add Barcode</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Product Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter product name" value={barcodeName} onChange={(e) => setBarcodeName(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="number" placeholder="Price" value={barcodePrice} onChange={(e) => setBarcodePrice(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="button" onClick={saveBarcode}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      }
+      </Row>
+    </Container>
   );
 }
 
